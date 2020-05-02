3bd4b23474d623384042962d74d1f6992f560684
Yuksel Umutlu <yumutlu@yandex.com>
2020-05-02 18:48:36 +0300

    Updating Context Data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import ThemeContextProvider from './components/contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        
+
       <Navbar/>
       <BookList/>
+      <ThemeToggle />
       </ThemeContextProvider>
-      
+    
     </div>
   );
 }
--- src/components/contexts/ThemeContext.js
+++ src/components/contexts/ThemeContext.js
@@ -3,14 +3,16 @@ export const ThemeContext = createContext();
 
 class ThemeContextProvider extends Component {
     state = {
-        isLightTh: true,
+        isLightTheme: true,
         light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
         dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
     }
+    toggleTheme = () => {
+        this.setState({ isLightTheme: !this.state.isLightTheme });
+    }
     render() {
         return (
-            <ThemeContext.Provider value={{ ...this.state }}>
-
+            <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
                 {this.props.children}
             </ThemeContext.Provider>
         );
